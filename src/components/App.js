38f044671450d45bf052cb061c79a376997141a4
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-08-12 14:10:43 -0400

    search is working so is axios promises to populate 80 requests from MetArtAPI data to set the state for it in order to pass it into props for MetArtMainComponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,125 @@
-import React, {Component, Fragment} from 'react'
-import axios from 'axios'
-import Search from './Search'
-import MetArtMain from './MetArtMain'
-
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+import Search from './Search';
+import MetArtMain from './MetArtMain';
 class App extends Component {
   state = {
     searchTermMetApi: '',
-    MetArtApiIDs: [] 
-  }
-
-  searchMetApi(value) {
-      this.setState({
-        searchTermMetApi: value
-      }, () => {
-        console.log(this.state);
-      })  
-      
+    MetArtApiData: []
+  };
+  searchMetApi = (event, value) => {
+    // console.log('value', value);
+    axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${value}`)
+    .then((foundAPIIds) => {
+      let arrayToRequestOfApiIds = [...foundAPIIds.data.objectIDs]
+      let fullListedData = []
+      // console.log(arrayToRequestOfApiIds);
+      if(arrayToRequestOfApiIds.length >= 80) {
+        for(let i = 0; i < 80; i++) {
+              axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayToRequestOfApiIds[i]}`)
+                    .then((individualFullObject) => {
+                      fullListedData.unshift(individualFullObject.data)
+                    }).catch((error) => console.log(error))
+        }
+      } else if (arrayToRequestOfApiIds.length < 80) {
+        for(let i = 0; i < arrayToRequestOfApiIds.length; i++) {
+          axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayToRequestOfApiIds[i]}`)
+                .then((individualFullObject) => {
+                  fullListedData.unshift(individualFullObject.data)
+                }).catch((error) => console.log(error))
     }
-  
-
-
-  componentDidMount() {
-    console.log('Mount App');
-    console.log(this.state);
-  }
-
+      }
+      console.log(fullListedData);
+      this.setState(
+        {
+          searchTermMetApi: value,
+          MetArtApiData: fullListedData
+        },
+        () => {
+          console.log('state', this.state);
+        }
+      );
+    })
+    .catch((error) => console.log(error))
+  };
+  // componentDidMount() {
+  //   console.log('Mount App');
+  //   console.log(this.state);
+  // }
   render() {
-    console.log('render App');
+    console.log('in render', this.state.searchTermMetApi);
     return (
-      
       <Fragment>
-      <div className="ui top fixed inverted menu">
-      <div className="ui container">
-
-        <a className="item" href="#root">Home</a>
-        <div className="ui simple dropdown item">
-          Dropdown <i className="dropdown icon"></i>
-          <div className="menu">
-            <a className="item" href="#root">Link Item</a>
-            <a className="item" href="#root">Link Item</a>
-            <div className="divider"></div>
-            <div className="header">Header Item</div>
-            <div className="item">
-              Sub Menu <i className="dropdown icon"></i>
-              <div className="menu">
-                <a className="item" href="#root">Link Item</a>
-                <a className="item" href="#root">Link Item</a>
+        <div className='ui top fixed inverted menu'>
+          <div className='ui container'>
+            <a className='item' href='#root'>
+              Home
+            </a>
+            <div className='ui simple dropdown item'>
+              Dropdown <i className='dropdown icon'></i>
+              <div className='menu'>
+                <a className='item' href='#root'>
+                  Link Item
+                </a>
+                <a className='item' href='#root'>
+                  Link Item
+                </a>
+                <div className='divider'></div>
+                <div className='header'>Header Item</div>
+                <div className='item'>
+                  Sub Menu <i className='dropdown icon'></i>
+                  <div className='menu'>
+                    <a className='item' href='#root'>
+                      Link Item
+                    </a>
+                    <a className='item' href='#root'>
+                      Link Item
+                    </a>
+                  </div>
+                </div>
+                <a className='item' href='#root'>
+                  Link Item
+                </a>
               </div>
             </div>
-            <a className="item" href="#root">Link Item</a>
           </div>
         </div>
-      </div>
-    </div>
-    <div className="ui main container">
-      <h1 className="ui header">Semantic UI Fixed Template</h1>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>
-        A text container is used for the main container, which is useful for
-        single column layouts
-      </p>
-
-      <Search searchMetApi={this.searchMetApi}/>
-      <MetArtMain />
-
-
-    </div>
-    <div className="ui inverted vertical footer segment">
-      <div className="ui center aligned container">
-        <div className="ui inverted section divider"></div>
-
-        <div className="ui horizontal inverted small divided link list">
-          <a className="item" href="https://github.com/EdmontDantes/term2-capstone-backend">Backend github</a>
-          <a className="item" href="https://github.com/EdmontDantes/term2-capstone-frontend">Frontend github</a>
-          <a className="item" href="https://github.com/EdmontDantes">Bogdan Kowaltchook</a>
+        <div className='ui main container'>
+          <h1 className='ui header'>Semantic UI Fixed Template</h1>
+          <p>
+            This is a basic fixed menu template using fixed size containers.
+          </p>
+          <p>
+            A text container is used for the main container, which is useful for
+            single column layouts
+          </p>
+          <Search searchMetApi={this.searchMetApi} />
+          <MetArtMain />
         </div>
-      </div>
-    </div>
+        <div className='ui inverted vertical footer segment'>
+          <div className='ui center aligned container'>
+            <div className='ui inverted section divider'></div>
+            <div className='ui horizontal inverted small divided link list'>
+              <a
+                className='item'
+                href='https://github.com/EdmontDantes/term2-capstone-backend'
+              >
+                Backend github
+              </a>
+              <a
+                className='item'
+                href='https://github.com/EdmontDantes/term2-capstone-frontend'
+              >
+                Frontend github
+              </a>
+              <a className='item' href='https://github.com/EdmontDantes'>
+                Bogdan Kowaltchook
+              </a>
+            </div>
+          </div>
+        </div>
       </Fragment>
-      )
+    );
   }
 }
-
-export default App
+export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,59 +1,71 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import Button from './Button'
-
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
 class Search extends Component {
-  constructor(){
-    super()
-    this.state={
-      searchTermMetApiSearchComponent: ''
-
-    }
+  constructor() {
+    super();
+    this.state = {
+      searchTermMetApiSearchComponent: '',
+      searchTermMetApi: ''
+    };
   }
-
-  onChangeSearchValue(event) {
+  onChangeSearchValue = (event) => {
     // event.preventDefault()
-    let newSearchTerm = event.target.value.trim()
-    console.log(newSearchTerm)
-    this.setState({
-      searchTermMetApi: newSearchTerm
-    }, () => {
-      console.log(this.state);
-    })
-  } 
-
-  componentDidMount() {
-
-  }
-
+    let newSearchTerm = event.target.value.trim();
+    console.log(newSearchTerm);
+    this.setState(
+      {
+        searchTermMetApi: newSearchTerm
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+  sendSearch = (event, value) => {
+    event.preventDefault();
+    this.props.searchMetApi(event, value);
+  };
+  componentDidMount() {}
   render() {
-
-    return(
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-        <form action="" className="ui form">
-        <div className="field" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-          <input 
-                type='text' 
-                defaultValue={this.state.searchTermMetApiSearchComponent} 
-                onChange={
-                  this.onChangeSearchValue.bind(this)
-
-                          }/>
-
-        <Button 
-                
-                className='ui red button' 
-                onClick={() => {
-                                this.props.searchMetApi(this.state.searchTermMetApiSearchComponent)
-                          }} children='Search Art' />
-  
-        </div>
-      </form>
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column'
+        }}
+      >
+        <form
+          onSubmit={(event) => {
+            this.sendSearch(event, this.state.searchTermMetApi);
+          }}
+          className='ui form'
+        >
+          <div
+            className='field'
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row'
+            }}
+          >
+            <input
+              type='text'
+              value={this.state.searchTermMetApi}
+              onChange={this.onChangeSearchValue}
+            />
+            <Button
+              type='submit'
+              className='ui red button'
+              children='Search Art'
+            />
+          </div>
+        </form>
       </div>
-    )
-
+    );
   }
 }
-
-export default Search
+export default Search;
