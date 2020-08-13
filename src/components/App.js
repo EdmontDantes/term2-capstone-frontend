dba5d03db32fd728ea8ca2c2d1fd7edf53c2efdf
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-08-13 08:57:10 -0400

    I totally broke everything but hey at least my data retrieved is blazing fast

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import { clone } from "lodash"
 class App extends Component {
   state = {
     searchTermMetApi: '',
-    MetArtApiData: [],
+    MetArtApiData: [{objectID: '1', primaryImageSmall: 'none', title: 'title', artistDisplayName: 'None'}],
     toggleMetArtLoading: false
   };
 
@@ -21,40 +21,53 @@ class App extends Component {
 
 
   searchMetApi = async (event, value) => {
-    event.preventDefault()
-    this.loadingShowMetArtAPIResults()
+    // event.preventDefault()
     // console.log('value', value);
     const fullListedData = []
     await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${value}`)
     .then(async (foundAPIIds) => {
-      const arrayToRequestOfApiIds = [...foundAPIIds.data.objectIDs]
+      this.loadingShowMetArtAPIResults()
+      let cutArrayIds = []
+      await (foundAPIIds.data.objectIDs.length > 80 
+        ? cutArrayIds = foundAPIIds.data.objectIDs.slice(0, 80) 
+        : cutArrayIds = foundAPIIds.data.objectIDs.slice(0, foundAPIIds.data.objectIDs.length))
+      // const arrayToRequestOfApiIds =foundAPIIds.data.objectIDs.map((value, idx) => idx <= 80)
+      // console.log('ArrayTOREquestOFApiIDS', arrayToRequestOfApiIds);
+      cutArrayIds.map(async (objectId, idx) => {
+        await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
+        .then(async (individualFullObject) => {
+          // console.log('Axios IndividualFullObject Console.log', individualFullObject)
+          fullListedData.push(individualFullObject.data)
+        }).catch((error) => console.log(error))
+      })
       // console.log(arrayToRequestOfApiIds);
-      if(foundAPIIds.data.objectIDs.length >= 80) {
-        for(let i = 0; i < 80; i++) {
-              await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayToRequestOfApiIds[i]}`)
-                    .then((individualFullObject) => {
-                      // console.log('Axios IndividualFullObject Console.log', individualFullObject)
-                      fullListedData.push(individualFullObject.data)
-                    }).catch((error) => console.log(error))
-        }
-      } else if (foundAPIIds.data.objectIDs.length < 80) {
-        for(let i = 0; i < arrayToRequestOfApiIds.length; i++) {
-          await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayToRequestOfApiIds[i]}`)
-                .then((individualFullObject) => {
-                  // console.log('Axios IndividualFullObject Console.log', individualFullObject)
-                  fullListedData.push(individualFullObject.data)
-                }).catch((error) => console.log(error))
-    }
-      }
-      this.setState({
-        searchTermMetApi: value,
-        MetArtApiData: fullListedData,
-        toggleMetArtLoading: false
-        })
-      // console.log('HELLO AFTER AXOIS PROMISES', fullListedData);
-    })
-    .catch((error) => console.log(error))
-    // console.log("After axios await promises", fullListedData);
+    //   if(foundAPIIds.data.objectIDs.length >= 80) {
+    //     for(let i = 0; i < 80; i++) {
+    //           await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayToRequestOfApiIds[i]}`)
+    //                 .then((individualFullObject) => {
+    //                   // console.log('Axios IndividualFullObject Console.log', individualFullObject)
+    //                   fullListedData.push(individualFullObject.data)
+    //                 }).catch((error) => console.log(error))
+    //     }
+    //   } else if (foundAPIIds.data.objectIDs.length < 80) {
+    //     for(let i = 0; i < arrayToRequestOfApiIds.length; i++) {
+          // await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayToRequestOfApiIds[i]}`)
+          //       .then((individualFullObject) => {
+          //         // console.log('Axios IndividualFullObject Console.log', individualFullObject)
+          //         fullListedData.push(individualFullObject.data)
+          //       }).catch((error) => console.log(error))
+    // }
+    //   }
+    console.log('HELLO AFTER AXOIS PROMISES', fullListedData);
+    
+  })
+  .catch((error) => console.log(error))
+  // console.log("After axios await promises", fullListedData);
+          this.setState({
+            searchTermMetApi: value,
+            MetArtApiData: fullListedData,
+            toggleMetArtLoading: false
+            })
   };
   // componentDidMount() {
   //   console.log('Mount App');
@@ -69,7 +82,7 @@ class App extends Component {
   // }
 
   render() {
-    // console.log('in render', this.state.MetArtApiData);
+    console.log('in render', this.state.MetArtApiData);
     return (
       <Fragment>
         <div className='ui top fixed inverted menu'>
@@ -110,6 +123,7 @@ class App extends Component {
           <h1 className='ui header'>The Metropolitan Museum of Art Collection API Widget</h1>
 
           <Search searchMetApi={this.searchMetApi}  btnType={'submit'} btnClassName={'ui red button'} btnChildren={'Search Art'}/>
+          <MetArtMain MetArtApiDataToComponent={this.state.MetArtApiData} toggleMetArtLoading={this.state.toggleMetArtLoading} />
           {this.state.toggleMetArtLoading ? (<MetArtMain MetArtApiDataToComponent={this.state.MetArtApiData} toggleMetArtLoading={this.state.toggleMetArtLoading} />) : (<MetArtMain MetArtApiDataToComponent={this.state.MetArtApiData} />) }
           
         </div>
--- src/components/MetArtMain.js
+++ src/components/MetArtMain.js
@@ -1,36 +1,49 @@
 import React from 'react'
 import MetArtTile from './MetArtTile'
-import { clone } from "lodash" 
+import { clone, cloneDeep } from "lodash" 
 
 const MetArtMain = (props) => {
 
-const savedArrayProps = props.MetArtApiDataToComponent
-    return (
-      <div className="ui raised very padded container segment">
-
-      <h1>Your The Metropolitan Museum of Art Collection API search results</h1>
-      {(props.toggleMetArtLoading === true) ?
-      (
-      <div className="ui active dimmer">
-        <div className="ui indeterminate text loader">Preparing Results</div>
-
-    </div>) : (<br />)}
-        <div className="ui five cards" style={{paddingBottom: '15px'}}>
+const savedArrayProps = cloneDeep(props.MetArtApiDataToComponent)
+  const functionToPerform =
     
-   
-      {savedArrayProps.map((individualObjectArt) => {
-        return(
-          <MetArtTile key={individualObjectArt.objectID} image={individualObjectArt.primaryImageSmall} title={individualObjectArt.title} artist={individualObjectArt.artistDisplayName}></MetArtTile>
-
-        )
-      })}
-        
-        </div>
-        <div className="ui pagination menu" style={{paddingTop: '15px'}}>
-
-        </div>
-      </div>
+    savedArrayProps.map((individualObjectArt) => {
+      return(
+        <MetArtTile key={individualObjectArt.objectID} image={individualObjectArt.primaryImageSmall} title={individualObjectArt.title} artist={individualObjectArt.artistDisplayName}></MetArtTile>
+  
       )
+    })
+  
+
+  return (
+    <div className="ui raised very padded container segment">
+
+    <h1>Your The Metropolitan Museum of Art Collection API search results</h1>
+    {(props.toggleMetArtLoading === true) ?
+    (
+    <div className="ui active dimmer">
+      <div className="ui indeterminate text loader">Preparing Results</div>
+
+  </div>) : (<br />)}
+      <div className="ui five cards" style={{paddingBottom: '15px'}}>
+  
+      {console.log('IN METARTMAIN', props.MetArtApiDataToComponent)}
+      {}
+
+      {functionToPerform}
+    {props.MetArtApiDataToComponent.map((individualObjectArt) => {
+      return(
+        <MetArtTile key={individualObjectArt.objectID} image={individualObjectArt.primaryImageSmall} title={individualObjectArt.title} artist={individualObjectArt.artistDisplayName}></MetArtTile>
+
+      )
+    })}
+      
+      </div>
+      <div className="ui pagination menu" style={{paddingTop: '15px'}}>
+
+      </div>
+    </div>
+    )
   
 
 }
